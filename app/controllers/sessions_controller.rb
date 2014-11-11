d0a935a8767d7b414aade531a1026c793f665c02
Peter Jang <peter.x.jang@gmail.com>
2014-11-11 00:34:12 -0600

    fixed bug with login incorrect email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 		p params
 		p User.all
     @user = User.find_by(email: params[:email])
-    if @user.authenticate(params[:password]) #@user && @user.password == params[:password]
+    if @user && @user.authenticate(params[:password]) #@user && @user.password == params[:password]
       session[:user_id] = @user.id
       redirect_to @user
     else
