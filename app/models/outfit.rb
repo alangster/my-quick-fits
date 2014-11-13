a312d34c71465534f512767cc2a3e659d9061ce2
Alex Lang <langalex4@gmail.com>
2014-11-12 18:02:53 -0600

    error messages correlated with specific articles

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -47,17 +47,34 @@ class Outfit < ActiveRecord::Base
     s
   end
 
+  def self.has_error?(results)
+    flag = results[:complementary] && 
+           results[:proper_dress_code] && 
+           results[:water_resistant] && 
+           results[:good_condition] &&
+           results[:clean]
+    !flag
+  end
+
+  def self.get_modified_name(article, results)
+    name = article.category.name
+    name += " *" if has_error?(results)
+    name
+  end
+
   def self.make_outfit(current_wardrobe, temperature, precipitation, formal)
 
     outfit_so_far = []
     errors = []
 
     tops = []
+    top_names = []
     current_layer = 0
     while current_layer < 4 do
       tops_current_layer = current_wardrobe.get_all_tops(current_layer)
       results = Article.get_appropriate_articles(tops_current_layer, temperature, precipitation, formal, outfit_so_far)
       tops << results[:articles].sample
+      top_names << get_modified_name(tops.last, results)
       break if results[:within_temp]
       current_layer += 1
       outfit_so_far << tops.last
@@ -67,14 +84,16 @@ class Outfit < ActiveRecord::Base
     bottoms = current_wardrobe.get_all_bottoms
     results = Article.get_appropriate_articles(bottoms, temperature, precipitation, formal, outfit_so_far)
     bottom_final = results[:articles].sample
+    bottom_name = get_modified_name(bottom_final, results)
     errors += outfit_error_messages(results)
     outfit_so_far << bottom_final
 
     shoes = current_wardrobe.get_all_shoes
     results = Article.get_appropriate_articles(shoes, temperature, precipitation, formal, outfit_so_far)
     shoes_final = results[:articles].sample
+    shoes_name = get_modified_name(shoes_final, results)
     errors += outfit_error_messages(results)
 
-    {tops: tops, bottom: bottom_final, shoes: shoes_final, errors: errors.uniq}
+    {tops: tops, top_names: top_names, bottom: bottom_final, bottom_name: bottom_name, shoes: shoes_final, shoes_name: shoes_name, errors: errors.uniq}
   end
 end
